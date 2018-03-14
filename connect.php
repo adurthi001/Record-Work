3148e489f1d5028e529f0370f86542d3478ff6a8
adurthi001 <36588182+adurthi001@users.noreply.github.com>
2018-03-14 23:32:19 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername="localhost";
-  $username="root";
-  $password="";
-  $db="memories";
-  $conn = mysqli_connect($servername,$username,$password)  or die("connection failed : ".mysql_connect());
-  mysqli_select_db($conn,$db);
-?>
